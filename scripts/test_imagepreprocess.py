1af6f787090e17d4e297ea4e9d0aebc291c15ff3
David Gitz <davidgitz@gmail.com>
2014-02-18 21:13:24 -0600

    More work on ANN pre-processing.

--- scripts/test_imagepreprocess.py
+++ scripts/test_imagepreprocess.py
@@ -15,6 +15,7 @@ image_paths = []
 (opts,args) = parser.parse_args()
 mainpath = os.getcwd()
 trainimage_dir = mainpath +'..\\..\\media\\RealImages\\'
+environment_dir = mainpath +'..\\..\\media\\EnvironmentImages\\'
 folders = os.listdir(trainimage_dir)
 for f in range(len(folders)):
 	mypath = trainimage_dir + '\\' + folders[f] + '\\'
@@ -22,6 +23,10 @@ for f in range(len(folders)):
 	myfiles = os.listdir(mypath)
 	for i in range(len(myfiles)):
 		image_paths.append(trainimage_dir + '\\' + folders[f] + '\\' + myfiles[i])
+myfiles = os.listdir(environment_dir)
+for i in range(len(myfiles)):
+	image_paths.append(environment_dir + '\\' + myfiles[i])
+
 
 def mainloop():
 	time.sleep(3)
@@ -29,11 +34,11 @@ def mainloop():
 	cv2.namedWindow("Original Image",1)
 	cv2.namedWindow("Processed Image",1)
 	cv2.namedWindow("Thresholded Image",1)
+	cv2.setMouseCallback("Processed Image",my_mouse_callback)
 	for i in range(len(image_paths)):
 	    #lasttime = curtime
 		#curtime = time.time()
 		orig_image = cv2.imread(image_paths[i])
-		time.sleep(.1)
 		new_image = preprocess(orig_image)
 		print 'Finished {}/{} Images'.format(i+1,len(image_paths))
 		#elapsedtime = (curtime-lasttime)
@@ -45,15 +50,26 @@ def mainloop():
 
 
 def initvariables():
-	dumb = 1
+	global mouse_x
+	global mouse_y
+	mouse_x = -1
+	mouse_y = -1
+
+def my_mouse_callback(event,x,y,flags,param):
+	global mouse_x
+	global mouse_y
+	mouse_x = x
+	mouse_y = y
 
 def preprocess(orig_image):
-    #new_image = orig_image
+	global mouse_x
+	global mouse_y
+
 	filter = np.uint8([[[0,0,255]]])
 	#contour_image = np.uint8([[[0,0,255]]])
 	height,width = orig_image.shape[:2]
 	
-	#new_image = np.zeros((height,width),np.uint8)
+	new_image = np.zeros((height,width,3),np.uint8)
 	#thresh_image = np.zeros((height,width),np.uint8)
 	#proc_image = np.zeros((height,width),np.uint8)
 	proc_image = cv2.cvtColor(orig_image,cv2.COLOR_BGR2HSV)
@@ -61,26 +77,35 @@ def preprocess(orig_image):
 	#proc_image = 255 - proc_image 
 	proc_image = cv2.cvtColor(proc_image,cv2.COLOR_BGR2GRAY)
 	proc_image = cv2.equalizeHist(proc_image)
-	(thresh,thresh_image) = cv2.threshold(proc_image,35,255,cv2.THRESH_BINARY)
-	erode_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-	proc_image = cv2.erode(proc_image,erode_kernel)
-	dilate_kernel = np.ones((5,5),'uint8')
-	proc_image = cv2.dilate(thresh_image,dilate_kernel)
+	(thresh,thresh_image) = cv2.threshold(proc_image,50,255,cv2.THRESH_BINARY_INV)
+	'''erode_kernel = np.ones((5,5),np.uint8)
+	hproc_image = cv2.erode(thresh_image,erode_kernel,iterations=5)
+	dilate_kernel = np.ones((1,1),np.uint8)
+	proc_image = cv2.dilate(proc_image,dilate_kernel)'''
+	new_image[...,0] = thresh_image * orig_image[...,0]
+	new_image[...,1] = thresh_image * orig_image[...,1]
+	new_image[...,2] = thresh_image * orig_image[...,2]
+
 	if 0:
-		contours,hierarchy = cv2.findContours(proc_image,1,2)
+		contours,hierarchy = cv2.findContours(proc_image,mode=cv2.RETR_EXTERNAL,method=cv2.CHAIN_APPROX_NONE)
 		cnt = contours[0]
+		for i in range(len(contours)):
+			if len(contours[i]) > len(cnt):
+				cnt = contours[i]
 		rect = cv2.minAreaRect(cnt)
 		box = cv2.cv.BoxPoints(rect)
 		box = np.int0(box)
-		cv2.drawContours(orig_image,[box],0,(0,0,255),2)
+		print len(cnt)
+		cv2.drawContours(orig_image,cnt,-1,(0,0,255),2)
+
 			
 	cv2.imshow("Original Image",orig_image)
 	cv2.waitKey(1)
 	cv2.imshow("Thresholded Image",thresh_image)
 	cv2.waitKey(1)
-	cv2.imshow("Processed Image",proc_image)
+	cv2.imshow("Processed Image",new_image)
 	cv2.waitKey(1)
-	return proc_image
+	return new_image
 
 if __name__ == '__main__':
         mainloop()
